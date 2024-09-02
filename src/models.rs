33c5b00fc579f456c3f932a9553e7c1de0e0f39b
lijiachang <944581577@qq.com>
2024-09-02 23:18:50 +0800

    diesel rename table name

--- src/models.rs
+++ src/models.rs
@@ -3,14 +3,18 @@
 #![allow(unused)]
 #![allow(clippy::all)]
 
+use crate::schema::fib_entries;
+use crate::schema::alembic_version;
 
 #[derive(Queryable, Debug, Identifiable)]
 #[diesel(primary_key(version_num))]
+#[diesel(table_name = alembic_version)] //自定义表名
 pub struct AlembicVersion {
     pub version_num: String,
 }
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable, Debug, Identifiable)]
+#[diesel(table_name = fib_entries)] //自定义表名
 pub struct FibEntry {
     pub id: i32,
     pub input_number: Option<i32>,
